e80b4b7d036ee0b404476165dda561a38b04a9b3
Ignacio Chiappe <nacho.chiappe@gmail.com>
2015-10-07 12:22:36 -0300

    Nueva versión provista por la cátedra.

--- lectura.c
+++ lectura.c
@@ -1,21 +1,24 @@
 #define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char* chomp(char* str) { 
+#include "lectura.h"
+
+char* chomp(char* str) {
 	if (*str && str[strlen(str)-1] == '\n') {
-		str[strlen(str)-1] = '\0'; 
+		str[strlen(str)-1] = '\0';
 	}
-	return str; 
+	return str;
 
-} 
+}
 
-char* leer_linea() {
+char* leer_linea(FILE *fp) {
 	size_t tam = 0;
 	char *linea = NULL;
 
-	if (getline(&linea, &tam, stdin) != -1) {
+	if (getline(&linea, &tam, fp) != -1) {
 		return chomp(linea);
 	} else {
 		free(linea);
--- lectura.h
+++ lectura.h
@@ -1,12 +1,23 @@
 #ifndef LECTURA_H
 #define LECTURA_H
 
+#include <stdio.h>
+
 /*
- * Lee una linea de la entrada estandar, hasta encontrar un caracter de fin
- * de linea o el fin de la entrada estandar.
- * Devuelve un char* con el contenido leido en memoria dinamica. Debe liberarse
- * con free() cuando no se necesite mas.
+ * Lee una línea de un archivo o entrada estándar hasta encontrar un caracter
+ * de fin de línea o el fin del archivo . Devuelve un char* con el contenido
+ * leído en memoria dinámica, o NULL cuando se llega al final del archivo.
+ * La cadena devuelta debe liberarse con free() cuando no se necesite más.
+ *
+ * Uso:
+ *
+ *    FILE *file = fopen(...);
+ *    char *linea = leer_linea(file);
+ *
+ * O, para entrada estándar:
+ *
+ *    char *linea = leer_linea(stdin);
  */
-char* leer_linea() ;
+char* leer_linea(FILE *fp);
 
 #endif // LECTURA_H
